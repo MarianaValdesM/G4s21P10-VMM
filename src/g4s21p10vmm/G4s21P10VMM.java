0aab0e0876f218235cc0c7838204993027866530
Mariana <>
2023-05-25 10:33:39 -0600

    Practica 10

---
+++ src/g4s21p10vmm/G4s21P10VMM.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4s21p10vmm;
+
+/**
+ *
+ * @author shack
+ */
+public class G4s21P10VMM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new jfPrincipal().setVisible(true);
+    }
+    
+}
